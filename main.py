112bfb7162c488d0eec240d0752a1e0a672d114c
Steven Belk <srbelk@gmail.com>
2025-03-09 16:03:59 -0400

    Setup the display screen

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            if pygame.init():
+                screen.fill((0,0,0))
+                pygame.display.flip()
+          
+
 
     print("Starting Asteroids!" )
     print(f"Screen width: {SCREEN_WIDTH}")
