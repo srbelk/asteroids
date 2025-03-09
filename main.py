f69c63ece9309061a57d9d3c589edc2be6b6b480
srbelk <srbelk@gmail.com>
2025-03-09 14:23:22 -0400

    Setup asteroids openning greeting + constant values for the game

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+#from database import connect_database, database_version
+from constants import *
+
+def main():
+
+    print("Starting Asteroids!" )
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
