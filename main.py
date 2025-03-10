0c9df335660b2e02d4f5706fb95d8df638266e94
srbelk <srbelk@gmail.com>
2025-03-10 16:29:46 -0400

    Setup FPS and DeltaTime

--- main.py
+++ main.py
@@ -5,16 +5,19 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = clock.tick(60) / 1000
     
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            if pygame.init():
-                screen.fill((0,0,0))
-                pygame.display.flip()
-          
-
+            
+        screen.fill((0,0,0))
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000    
+                
 
     print("Starting Asteroids!" )
     print(f"Screen width: {SCREEN_WIDTH}")
