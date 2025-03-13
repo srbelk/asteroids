90ae466c15ccf5a72f8878ba8eecd2d8df8906d5
srbelk <srbelk@gmail.com>
2025-03-13 12:22:11 -0400

    Setup asteroid sprites and movement

--- main.py
+++ main.py
@@ -3,16 +3,32 @@ import pygame
 from constants import *
 from circleshape import *
 from player import *
+from asteroid import Asteroid
+from asteroidfield import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     clock = pygame.time.Clock()
+    
     dt = 0
+    
     updatable = pygame.sprite.Group()
+    
     drawable = pygame.sprite.Group() 
+    
     Player.containers = (updatable, drawable)
+    
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+    AsteroidField.containers = (updatable)
+
+    asteroidfield = AsteroidField()    
 
     while True:
         for event in pygame.event.get():
